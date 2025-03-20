476cc73cefe12248b63f694b1a62bfe4c7675d78
Yusuf Puluco <yusuf.puluco@gmail.com>
2025-03-20 16:16:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/sistem-booking-lab/', // Tambahkan base URL sesuai nama repository,
   server: {
     port: 3000,
     strictPort: false,
